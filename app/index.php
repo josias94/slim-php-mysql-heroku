cf486fe853a2bb8c37a01d13af55d539c3e96220
josias94 <josiasrivola@gmail.com>
2021-06-10 17:48:50 -0300

    asd

--- app/index.php
+++ app/index.php
@@ -19,6 +19,8 @@ require_once './controllers/ProductoController.php';
 require_once './controllers/MesaController.php';
 require_once './controllers/PedidoController.php';
 
+require './middlewares/MWparaAutentificar.php';
+
 // Load ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
@@ -31,21 +33,25 @@ $app->addErrorMiddleware(true, true, true);
 
 
 // Routes
+// $app->group("[/]",function (RouteCollectorProxy $group1){
+  
+  
+//   $group1->group('/productos', function (RouteCollectorProxy $group) {
+//     $group->get('[/]', \ProductoController::class . ':TraerTodos');
+//     $group->get('/id={id}', \ProductoController::class . ':TraerUno');
+//     $group->post('[/]', \ProductoController::class . ':CargarUno');
+//     $group->post('/Modificar', \ProductoController::class . ':ModificarUno');
+//     $group->delete('/id={productoId}', \ProductoController::class . ':BorrarUno');
+//   })->add(\MWparaAutentificar::class . ':VerificarUsuario');
+// });
+
 $app->group('/usuarios', function (RouteCollectorProxy $group) {
   $group->get('[/]', \UsuarioController::class . ':TraerTodos');
   $group->get('/id={id}', \UsuarioController::class . ':TraerUno');
   $group->post('[/]', \UsuarioController::class . ':CargarUno');
   $group->post('/Modificar', \UsuarioController::class . ':ModificarUno');
   $group->delete('/id={usuarioId}', \UsuarioController::class . ':BorrarUno');
-});
-
-$app->group('/productos', function (RouteCollectorProxy $group) {
-  $group->get('[/]', \ProductoController::class . ':TraerTodos');
-  $group->get('/id={id}', \ProductoController::class . ':TraerUno');
-  $group->post('[/]', \ProductoController::class . ':CargarUno');
-  $group->post('/Modificar', \ProductoController::class . ':ModificarUno');
-  $group->delete('/id={productoId}', \ProductoController::class . ':BorrarUno');
-});
+})->add(\MWparaAutentificar::class . ':VerificarUsuario');
 
 $app->group('/mesas', function (RouteCollectorProxy $group) {
   $group->get('[/]', \MesaController::class . ':TraerTodos');
@@ -53,17 +59,17 @@ $app->group('/mesas', function (RouteCollectorProxy $group) {
   $group->post('[/]', \MesaController::class . ':CargarUno');
   $group->post('/Modificar', \MesaController::class . ':ModificarUno');
   $group->delete('/id={codIdentificacion}', \MesaController::class . ':BorrarUno');
-});
+})->add(\MWparaAutentificar::class . ':VerificarUsuario');
 
 $app->group('/pedidos', function (RouteCollectorProxy $group) {
   $group->get('[/]', \PedidoController::class . ':TraerTodos');
   $group->get('/id={id}', \PedidoController::class . ':TraerUno'); 
   $group->post('[/]', \PedidoController::class . ':CargarUno');
   $group->post('/Modificar', \PedidoController::class . ':ModificarUno');
-  $group->delete('/id={PedidoId}', \PedidoController::class . ':BorrarUno');
+  $group->delete('/{id}', \PedidoController::class . ':BorrarUno');
   //Filtros
   //$group->get('/mesa={idMesa}', \PedidoController::class . ':TraerTodosFilterByMesa');
-});
+})->add(\MWparaAutentificar::class . ':VerificarUsuario');
 
 $app->get('[/]', function (Request $request, Response $response) { 
   $response->getBody()->write("Bienvenido a la Comanda");
